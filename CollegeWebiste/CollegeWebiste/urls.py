930c21f564151831a51ffbf33658d45ddad217cc
anilchinchawade <anilchinchawade@orchidengg.ac.in>
2021-12-03 23:12:10 +0530

    Updated all pages and added base page. Added carasoul to Home page

--- CollegeWebiste/CollegeWebiste/urls.py
+++ CollegeWebiste/CollegeWebiste/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
+admin.site.site_header = "Orchid Admin"
+admin.site.site_title = "Orchid Admin Portal"
+admin.site.index_title = "Welcome to Orchid Placement Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('placements/',include('placements.urls')),
+    path('',include('placements.urls')),
 ]
+
