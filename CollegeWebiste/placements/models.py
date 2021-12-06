b48f4d7ed94c45ab071a14391fede45a23931f0e
anilchinchawade <anilchinchawade@orchidengg.ac.in>
2021-12-06 20:10:02 +0530

    Migrations Done

--- CollegeWebiste/placements/models.py
+++ CollegeWebiste/placements/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class Student(models.Model):
-    first_name = models.CharField(max_length=25)
-    last_name = models.CharField(max_length=30)
-    contact = models.IntegerField()
-    email = models.EmailField(max_length=50)
-    age = models.IntegerField()
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    emailid =models.EmailField(max_length=100)
+    phoneno = models.IntegerField()
+    remarks = models.EmailField(max_length=1000)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
 
 
--- CollegeWebiste/placements/views.py
+++ CollegeWebiste/placements/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from datetime import datetime
+from .models import Contact
+from django.contrib import messages
 
 
 # Create your views here.
@@ -14,6 +17,16 @@ def services(request):
     return render(request,'services.html')
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        emailid = request.POST.get('emailid')
+        phoneno = request.POST.get('phoneno')
+        remarks = request.POST.get('remarks')
+        date = datetime.today()
+        contact = Contact(name=name,emailid=emailid,phoneno=phoneno,remarks=remarks,date=date)
+        contact.save()
+        messages.success(request,"Your information and remarks have been reched to us!! Thank You!!! ")
+
     return render(request,'contact.html')
 
 
